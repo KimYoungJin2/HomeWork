e693440c02e25bf17681f61027a6987b096bb414
니나니모 <carl2005@naver.com>
2023-05-26 12:54:16 +0900

    아직 나누지못한것

---
+++ ConsoleGameMain0526/Player.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <conio.h>
+
+class Player
+{
+public:
+    // 레퍼런스는 기본적으로 8바이트 참조입니다.
+    // int4를 그냥 16바이트입니다.
+    // int4&를 그냥 8바이트입니다.
+    // int4*를 그냥 8바이트입니다.
+
+    // 내가 넣으려는 인자가 합쳐서 8바이트가 넘어갈때
+    // 그걸 구조체나 클래스로 묶어서 참조형태로 넘기는게 대부분의 상황에서
+    // 무조건 이득이다.
+
+    int4 GetPos() const
+    {
+        return Pos;
+    }
+
+    void AddPos(const int4& _Pos)
+    {
+        Pos += _Pos;
+    }
+
+
+    void SetPos(const int4& _Pos)
+    {
+        // 뭔가가 그냥 거저 된다.
+        // => 내가 만들었다.
+        // => 컴파일러나 기본문법으로 생략되어 있다.
+        Pos = _Pos;
+    }
+
+    // 생성자에는 붙일수가 없다.
+    Player()
+        : Pos()
+    {
+    }
+
+    Player(const Player&) = delete;
